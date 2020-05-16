2a1b2e2d3b298b292f8e9485ddf6b18b6c1aec40
Greg Merrill <greg.merrill@roostergrin.com>
2020-05-15 17:11:51 -0700

    request(index.php): add birdEye widget codes - ticket(#34576)

--- dist/index.php
+++ dist/index.php
@@ -32,5 +32,32 @@
     try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
     catch(e){window.attachEvent("onload", $buo_f)}
   </script>
+<!-- Review Feed Rotating Widget Code -->
+<script type="text/javascript" src="https://birdeye.com/embed/v4/158932203595021/8/1234567985183"></script>
+<div id="bf-revz-widget-1234567985183">
+  <div class="bf-dv">
+    <span class="bf-spn"> powered by <a class="bf-pwr" href="http://birdeye.com" target="_blank">BirdEye</a></span>
+  </div>
+</div>
+<script>
+let checkingInterval = setInterval(myTimer, 1000);
+function myTimer() {
+	let revWidget = document.getElementById('bf-revz-widget-1234567985183')
+	if (!!revWidget) {
+    revWidget.style.position = 'fixed';
+		revWidget.style.bottom = '20px';
+		revWidget.style.left = '20px';
+    myStopFunction();
+    }
+}
+function myStopFunction() {
+	clearInterval(checkingInterval)
+}
+</script>
+<!-- Review Feed Rotating Widget Code -->
+<!-- Webchat Code -->
+<script type="text/javascript" src="https://birdeye.com/embed/v1/158932203595021/1/2011845117/EPmdsV4sDuAbU7knLOPrgb0phP13VIAK"></script>
+<div id="bf-revz-widget-2011845117"></div>
+<!-- Webchat Code -->
 </body>
 </html>
